86364bc1b117f13b308e4a43260cebe69c66bbe5
Rachit-Thakuriya07 <rachit77215@gmail.com>
2026-03-16 22:42:59 +0530

    UC4: Display Hello for multiple command line arguments or default World

---
+++ UC4.java
@@ -0,0 +1,14 @@
+public class UC4 {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        else {
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        }
+
+    }
+}
